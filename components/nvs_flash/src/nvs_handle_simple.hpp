b31bff7abf72cefcb7e08c1a51b010e4c199844a
Cody P Schafer <cschafer@weber.com>
2023-04-03 16:48:02 -0400

    nvs: support iteration over namespace by index
    Users of the nvs API are likely to have `nvs_handle_t` in all cases, but
    not all of them carry around the partition name and namespace name (as
    they aren't needed after creating an `nvs_handle_t`).
    
    Allow this common case to use nvs iteration without them tracking
    additional data by using the `nvs_handle_t` to locate the namespace
    index and the partition name by introducing an alterate to
    `nvs_entry_find` called `nvs_entry_find_in_handle`, which operates
    similarly except that it is given a `nvs_handle_t` instead of partition
    and namespace strings.
    
    This is somewhat more limited than the `nvs_entry_find` API as one
    cannot examine all keys in a given partition.

--- components/nvs_flash/src/nvs_handle_simple.hpp
+++ components/nvs_flash/src/nvs_handle_simple.hpp
@@ -1,16 +1,8 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #ifndef NVS_HANDLE_SIMPLE_HPP_
 #define NVS_HANDLE_SIMPLE_HPP_
 
@@ -74,9 +66,13 @@ public:
 
     bool findEntry(nvs_opaque_iterator_t *it, const char *name);
 
+    bool findEntryNs(nvs_opaque_iterator_t *it);
+
     bool nextEntry(nvs_opaque_iterator_t *it);
 
     const char *get_partition_name() const;
+
+    Storage *get_storage() const;
 
 private:
     /**
--- components/nvs_flash/src/nvs_storage.hpp
+++ components/nvs_flash/src/nvs_storage.hpp
@@ -1,16 +1,8 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #ifndef nvs_storage_hpp
 #define nvs_storage_hpp
 
@@ -132,6 +124,8 @@ public:
     esp_err_t calcEntriesInNamespace(uint8_t nsIndex, size_t& usedEntries);
 
     bool findEntry(nvs_opaque_iterator_t*, const char* name);
+
+    bool findEntryNs(nvs_opaque_iterator_t*, uint8_t nsIndex);
 
     bool nextEntry(nvs_opaque_iterator_t* it);
 
